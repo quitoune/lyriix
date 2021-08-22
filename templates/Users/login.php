b4ac6810a34437e304addb303401d2ea44002cf9
quitoune <42045951+quitoune@users.noreply.github.com>
2021-08-22 11:42:48 +0200

    add authentication

--- templates/Users/login.php
+++ templates/Users/login.php
@@ -3,13 +3,16 @@
  * @var \App\View\AppView $this
  */
 ?>
-<div class="users index content">
-    <h3><?= __('Login') ?></h3>
-    <div class="table-responsive">
-        <?= $this->Form->create() ?>
-        <?= $this->Form->control('pseudo') ?>
-        <?= $this->Form->control('password') ?>
-        <?= $this->Form->button('Connexion') ?>
-        <?= $this->Form->end() ?>
-    </div>
+<div class="users form">
+    <?= $this->Flash->render() ?>
+    <h3>Connexion</h3>
+    <?= $this->Form->create() ?>
+    <fieldset>
+        <?= $this->Form->control('pseudo', ['required' => true]) ?>
+        <?= $this->Form->control('password', ['required' => true]) ?>
+    </fieldset>
+    <?= $this->Form->submit(__('Login')); ?>
+    <?= $this->Form->end() ?>
+
+    <?= $this->Html->link("S'inscrire", ['action' => 'add']) ?>
 </div>
